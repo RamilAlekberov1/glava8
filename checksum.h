c4c2c389cd7baf979ae7bfd7137002d694fd7816
geo <eksars122@mail.ru>
2024-02-06 04:39:32 +0000

    glava8

---
+++ checksum.h
@@ -0,0 +1,6 @@
+#ifndef __checksum_h
+#define __checksum_h
+
+int checksum(char* message);
+
+#endif
---
+++ encrypt.h
@@ -0,0 +1,6 @@
+#ifndef __encrypt_h
+#define __encrypt_h
+
+void encrypt(char* message);
+
+#endif
